20cdeee5414934f82440b1a38ccb33c850c25a66
Lindsay <feilindsay@gmail.com>
2018-09-04 20:55:43 -0400

    binary test cases

--- testVectorsShift.py
+++ testVectorsShift.py
@@ -2,13 +2,10 @@ def shiftZero():
 	f = open('test_shift.txt','w')
 	f.write("B[32] Sa[5] Cin C[32] \n")
 	for i in [0, 1, 2, 3]:
-		b = i
+		b = bin(i)[2:].rjust(32, '0')
 		for j in range(32):
 			sa = str(j)
-			temp = j
-			cOut = b
-			while temp > 0:
-				cOut *= 2
-				temp -= 1
+			cOut = b[j:].ljust(32, '0')
+
 			f.write(str(b) + ' ' + sa + ' 0 ' + str(cOut) + '\n')
 	f.close()
